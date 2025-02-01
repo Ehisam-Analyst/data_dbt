70cef10b7a24f7914b7d99db621f6d32c39b479d
Clara Chukwudi <kennychuks.cic@gmail.com>
2025-02-01 23:46:20 +0100

    created view discounts_1001

--- models/staging/discounts_10001.sql
+++ models/staging/discounts_10001.sql
@@ -1,3 +1,3 @@
-with discount_10001 as (select rate_pct*0.01 from pora-academy-cohort3.jubilee_groceries.promotion
+with discount_10001 as (select rate_pct*0.01 as discount_3 from pora-academy-cohort3.jubilee_groceries.promotion
 where promo_code = 10001)
 select * from discount_10001
